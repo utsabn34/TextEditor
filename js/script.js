cf39ef91878d7b0911a5fc1f8de679ce952c1686
utsabn34 <utsabn34@gmail.com>
2014-12-07 18:35:07 +0545

    completion of phase 2

--- js/script.js
+++ js/script.js
@@ -1,91 +1,79 @@
 // JavaScript Document
-var editor=document.getElementById("editor");
+var start=new Editor();
 
-//change font style
-function changeFont(txt,change) {
-		var editor=document.getElementById(txt);
-		//for bold
-		if (change == 'bold') {
-            if (editor.style.fontWeight == 'bold')
-                editor.style.fontWeight = 'normal';
-            else
-                editor.style.fontWeight = 'bold';
-        }
-		//for italic
-        else if (change == 'italic') {
-            if (editor.style.fontStyle == 'italic')
-                editor.style.fontStyle = 'normal';
-            else
-                editor.style.fontStyle = 'italic';
-        }
-		//for underline
-        else if (change=='underline'){
-            if (editor.style.textDecoration == 'underline')
-                editor.style.textDecoration = 'none';
-            else
-                editor.style.textDecoration = 'underline';
-        }
-		//for fontsize
-		else if (change=='fontSize'){
-			var fsize=document.getElementById("fsize");
-			var fontSize=fsize.value;
-			editor.style.fontSize=fontSize+"px";
-		}
-		//for adjust right
-		else if (change=='adjustR'){
-			if(editor.style.textAlign=="right")
-				editor.style.textAlign="left";
-			else
-				editor.style.textAlign="right";
-		}
-		//for adjust center
-		else if (change=='adjustC'){
-			if(editor.style.textAlign=="right" || editor.style.textAlign=="left" )
-				editor.style.textAlign="center";
-			else
-				editor.style.textAlign="left";
-		}
-		//for adjust left
-		else if (change=='adjustL'){
-			editor.style.textAlign="left";
-		}
-		//for  font family
-		else if (change=='fontFamily'){
-			var fontFamily=document.getElementById("fontFamily");			
-			var value=fontFamily.value;
-			if(value==1){
-				editor.style.fontFamily="Times New Roman";
-			}
-			if(value==2){
-				editor.style.fontFamily="Arial";
-			}
-			if(value==3){
-				editor.style.fontFamily="Verdana, Geneva, sans-serif";
-			}
-			
-		}
-    }
-//select text from texarea
-function ShowSelectionInsideTextarea(editor){
+function frameOn(){
+	frame.document.designMode='On';
+	}
 
-  var textComponent = document.getElementById(editor);
-  var selectedText;
-  // IE version
-  if (document.selection != undefined)
-  {
-    textComponent.focus();
-    var sel = document.selection.createRange();
-    selectedText = sel.text;
-  }
-  // Mozilla version
-  else if (textComponent.selectionStart != undefined)
-  {
-    var startPos = textComponent.selectionStart;
-    var endPos = textComponent.selectionEnd;
-    selectedText = textComponent.value.substring(startPos, endPos)
-  }
-    
-	console.log(selectedText);
+function Editor(){
+	console.log("hello");
+	//for bold
+	var bold=document.getElementById("bold");
+	bold.onclick=function(){
+		frame.document.execCommand("bold",false,null);
+	}
+	//for italic
+	var italic=document.getElementById("italic");
+	italic.onclick=function(){
+		frame.document.execCommand("italic",false,null);
+	}
+	//for underline
+	var underline=document.getElementById("underline");
+	underline.onclick=function(){
+		frame.document.execCommand("underline",false,null);
+	}
+	//for text size
+	var txtSize=document.getElementById("txtSize");
+	txtSize.onclick=function(){
+		var size=prompt("Enter text Size","");
+		frame.document.execCommand("FontSize",false,size);
+	}
+	//for text color
+	var txtColor=document.getElementById("txtColor");
+	txtColor.onclick=function(){
+		var color=prompt("Enter name of text Color or hexadecimal code ","");
+		frame.document.execCommand("ForeColor",false,color);
+	}
+	//for horizontal line
+	var hr=document.getElementById("hr");
+	hr.onclick=function(){
+		frame.document.execCommand("inserthorizontalrule",false,null);
+	}
+	//for unordered list
+	var ul=document.getElementById("ul");
+	ul.onclick=function(){
+		frame.document.execCommand("InsertUnorderedList",false,"NewUL");
+	}
+	//for ordered list
+	var ol=document.getElementById("ol");
+	ol.onclick=function(){
+		frame.document.execCommand("InsertOrderedList",false,"NewOL");
+	}
+	//for setting link
+	var link=document.getElementById("link");
+	link.onclick=function(){
+		var Url=prompt("Enter link to insert ","http://");
+		frame.document.execCommand("CreateLink",false,Url);
+	}
+	//for unlink
+	var unlink=document.getElementById("unlink");
+	unlink.onclick=function(){
+		frame.document.execCommand("UnLink",false,null);
+	}
+	//to add image
+	var img=document.getElementById("image");
+	img.onclick=function(){
+		var src=prompt("Enter Image Location ","");
+		if(src!=null)
+		frame.document.execCommand("insertimage",false,src);
+	}
+	
+	var save=document.getElementById("save");
+	save.onclick=function(){
+		var theform=document.getElementById("myform");
+		theform.elements("editor").value=windows.frames("frame").document.body.innerHTML;
+		theform.submit();
+	}
+	
 }
-
 
